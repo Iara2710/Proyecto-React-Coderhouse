a74a1e9664f93ea4f3c65a5edb4fbe346119ce5a
Iara Graells <iaragraells@gmai.com>
2023-11-18 20:22:26 -0300

    creando pagina principal

---
+++ miproyecto-react/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import "./Banner.css"
+
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <h1>Funkopops shop</h1>
+            <p>¡Encontrá a tus personajes favoritos!</p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
